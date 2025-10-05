5797a2f902997c097e4137e2ee54568b3fdca08b
Ivan <164744807+diapazxn@users.noreply.github.com>
2025-10-05 19:24:01 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+age=16
+height=1.65
+name='Ivan'
+friends=['Vlad','Vitalik','Roman']
+grandparents=('grandmother','grandfather')
+student= True
+none= None
+numbers= {1,2,3,4,5,6}
+explanation={'Ivan': 'Student'}
+equation= 10-4j
+print(age)
+print(height)
+print(name)
+print(friends)
+print(grandparents)
+print(student)
+print(none)
+print(numbers)
+print(explanation)
+print(equation)
+print(type(age))
+print(type(height))
+print(type(name))
+print(type(friends))
+print(type(grandparents))
+print(type(student))
+print(type(none))
+print(type(numbers))
+print(type(explanation))
+print(type(equation))
